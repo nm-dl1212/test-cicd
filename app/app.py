571d7b056d6ff310a7993ea9ebbcc7e4f441d787
nmatsu <n.matsumura2022@gmail.com>
2025-05-11 23:36:34 +0900

    typo

--- app/app.py
+++ app/app.py
@@ -7,4 +7,4 @@ def hello_world():
     return jsonify({"message": "hello-world"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=True, host='0.0.0.0', port=5000)
