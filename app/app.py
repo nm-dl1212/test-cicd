10730415d85830d855bf640acc22d8e118664296
nmatsu <n.matsumura2022@gmail.com>
2025-05-17 16:40:16 +0900

    mod: calc endpointを追加

--- app/app.py
+++ app/app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def hello_world():
     return jsonify({"message": "hello-world"})
 
+@app.route('/calc')
+def calculation():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+
+    if a is None or b is None:
+        return jsonify({"error": "Parameters 'a' and 'b' are required."}), 400
+
+    sum_result = a + b
+    return jsonify({"message": f"sum is {sum_result}"})
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
